cacfb3000954175b7636b1295d6be2612f37ed58
Lorenzo Spinnato <lorenzito1999xx99@gmail.com>
2016-12-23 13:07:25 +0100

    Add files via upload
    Algoritmo di Ordinamento - Counting_Sort

---
+++ Counting Sort/Counting Sort/Counting Sort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Sort
+{
+    class Counting_Sort:Sorting
+    {
+        public int[] Sort(int[] vett)
+        {
+            return vett;
+        }
+
+        public void CountingSort(ref int[] data, int min, int max) //Viene preso come parametro il riferimento del vettore, la variabile intero
+        {
+            int[] count = new int[max - min + 1]; //Viene creato un nuovo vettore di dimensioni (valore massimo - valore minimo + 1)
+            int z = 0;
+
+            for (int i = 0; i < count.Length; i++) //Viene riempito il valore tenendo conto delle ripetizioni
+            {
+                count[i] = 0;
+            }
+            for (int i = 0; i < data.Length; i++) //Viene riordinato il vettore sommando le volte che il numero è stato ripetuto con il valore della cella precedente
+            {
+                count[data[i] - min]++;
+            }
+            for (int i = min; i <= max; i++) //I valori ottenuti vengono spostati in un terzo vettore
+            {
+                while (count[i - min]-- > 0)
+                {
+                    data[z] = i;
+                    ++z;
+                }
+            }
+        }
+    }
+}
---
+++ Counting Sort/Counting Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counting_Sort cs = new Counting_Sort(); 
+            Random r = new Random();
+            int s=0; //Intero temporaneo
+            int[] vett = new int[10000]; //Viene creato il vettore di 10000 celle
+            Console.WriteLine("Ordinando Il Vettore . . . ");
+           
+            for (int c = 0; c <= 10000; c++)
+            {
+                for (int i = 0; i < vett.Length; i++) //Viene riempito il vettore con la funzione Random
+                {
+                    vett[i] = r.Next(0, 99999);
+                }
+                var watch = System.Diagnostics.Stopwatch.StartNew(); //Avvio del Timer
+                {
+                    cs.CountingSort(ref vett, 0, 99999); //Ordinamento delle celle tenendo conto del valore minimo e del valore massimo
+                }
+                watch.Stop(); //Stop del Timer
+                var elapsedMs = watch.ElapsedMilliseconds; //Viene salvato in una variabile il Timer
+                s = s + Convert.ToInt32(elapsedMs); //Il Timer viene convertito in un intero a 64bit
+            }
+            
+        
+            for (int i = 0; i < vett.Length; i++) //Viene stampato il vettore ordinato
+            {
+                Console.WriteLine("Cella " + i + " : " + vett[i]);
+            }
+            Console.ReadLine();
+            Console.WriteLine("Tempo Medio d'Ordinamento: " + s/1000 + " ms"); //Stampa del Tempo Medio di Esecuzione dell'Algoritmo d'Ordinamento
+            Console.ReadLine();
+        }
+    }
+}
